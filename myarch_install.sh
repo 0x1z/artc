8547a00e60f74d6a0d156851e57700b09a5db4c2
Zero <38763440+0x1z@users.noreply.github.com>
2022-03-29 18:54:55 +0530

    Update myarch_install.sh

--- myarch_install.sh
+++ myarch_install.sh
@@ -155,5 +155,4 @@ sudo pacman -S --noconfirm xorg sddm plasma kde-applications firefox simplescree
 
 sudo systemctl enable sddm
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
-sleep 5
 exit

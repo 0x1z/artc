a86e399903cabc36cbe17fb415adefa2c698789c
Zero <38763440+0x1z@users.noreply.github.com>
2022-03-30 13:11:49 +0530

    Update post.sh

--- post.sh
+++ post.sh
@@ -1,3 +1,17 @@
 sudo firewall-cmd --add-port=1025-65535/tcp --permanent
 sudo firewall-cmd --add-port=1025-65535/udp --permanent
 sudo firewall-cmd --reload
+
+
+
+
+
+
+
+
+
+
+#command to comment no sudo password
+#sudo sed -i 's/^%wheel ALL=(ALL) NOPASSWD: ALL/#%wheel ALL=(ALL) NOPASSWD: ALL/g' /etc/sudoers
+#append without NOPASSWD
+#echo "%wheel ALL=(ALL): ALL" >> /etc/sudoers

b4f1918946f3c3a5caf0b46afb7dbca5b11cc782
Zero <38763440+0x1z@users.noreply.github.com>
2022-03-30 22:35:22 +0530

    Update post.sh

--- post.sh
+++ post.sh
@@ -5,12 +5,12 @@ sudo firewall-cmd --reload
 #pacman packages
 sudo pacman -S zsh zsh-completions neovim nodejs npm libreoffice mpv sxiv youtube-dl
 
+#changing shell
+sudo chsh -s /bin/zsh
 
 #yt-dlp installation
 sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
 sudo chmod a+rx /usr/local/bin/yt-dlp
-
-
 
 
 # oh-myzsh
@@ -23,12 +23,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 
-
-#adding to .zshrc
-echo "Enter Username: "
-read username
-sed -i -e 's/plugins=(.*)/plugins=(git archlinux zsh-syntax-highlighting zsh-autosuggestions)/' /home/$username/.zshrc
-sed -i -e "s/ZSH_THEME=\".*\"/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g" /home/$username/.zshrc
 
 #command to comment no sudo password
 #sudo sed -i 's/^%wheel ALL=(ALL) NOPASSWD: ALL/#%wheel ALL=(ALL) NOPASSWD: ALL/g' /etc/sudoers

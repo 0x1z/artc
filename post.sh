21fcc54dbbf3e85f2ce155ace7313be5a494c9d0
Zero <38763440+0x1z@users.noreply.github.com>
2022-03-30 15:57:40 +0530

    Update post.sh

--- post.sh
+++ post.sh
@@ -2,6 +2,8 @@ sudo firewall-cmd --add-port=1025-65535/tcp --permanent
 sudo firewall-cmd --add-port=1025-65535/udp --permanent
 sudo firewall-cmd --reload
 
+#pacman packages
+sudo pacman -S zsh zsh-completions
 
 
 
@@ -10,6 +12,8 @@ sudo firewall-cmd --reload
 
 
 
+# oh-myzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #command to comment no sudo password
 #sudo sed -i 's/^%wheel ALL=(ALL) NOPASSWD: ALL/#%wheel ALL=(ALL) NOPASSWD: ALL/g' /etc/sudoers

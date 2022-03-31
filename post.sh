adac8bad66a69a2d68af2a3ed56431a0bbf80392
Zero <38763440+0x1z@users.noreply.github.com>
2022-03-31 13:15:17 +0530

    Update post.sh

--- post.sh
+++ post.sh
@@ -8,11 +8,6 @@ sudo pacman --noconfirm -S zsh zsh-completions neovim nodejs npm libreoffice mpv
 #changing shell
 sudo chsh -s /bin/zsh
 
-#adding .zshrc
-echo "Enter Username: "
-read username
-
-touch /home/$username/.zshrc
 
 #yt-dlp installation
 sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
@@ -27,15 +22,3 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 #powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-#adding plugin and extention to .zshrc
-sed -i -e 's/plugins=(.*)/plugins=(git archlinux zsh-syntax-highlighting zsh-autosuggestions)/' /home/$username/.zshrc
-sed -i -e "s/ZSH_THEME=\".*\"/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g" /home/$username/.zshrc
-
-
-#append without NOPASSWD
-#sudo echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
-
-#command to comment no sudo password
-#sudo sed -i 's/^%wheel ALL=(ALL) NOPASSWD: ALL/#%wheel ALL=(ALL) NOPASSWD: ALL/g' /etc/sudoers
-

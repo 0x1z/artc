b1bc4be25e68d6ff40baca35f48f93070888ee9d
Zero <38763440+0x1z@users.noreply.github.com>
2022-03-31 12:45:29 +0530

    Delete zshrc.sh

--- zshrc.sh
+++
@@ -1,5 +0,0 @@
-#adding to .zshrc
-echo "Enter Username: "
-read username
-sed -i -e 's/plugins=(.*)/plugins=(git archlinux zsh-syntax-highlighting zsh-autosuggestions)/' /home/$username/.zshrc
-sed -i -e "s/ZSH_THEME=\".*\"/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g" /home/$username/.zshrc

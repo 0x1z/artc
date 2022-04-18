19385f7f5c9115afd33ecbfb93a6c2f4a881a882
Zero <38763440+0x1z@users.noreply.github.com>
2022-04-18 11:58:24 +0530

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -155,7 +155,7 @@ sudo pacman -S --noconfirm xorg sddm plasma kde-applications firefox simplescree
 
 sudo systemctl enable sddm
 
-yay --noconfirm -S layan-kde-git spotify-adblock google-chrome brave-bin visual-studio-code-bin sublime-text-4 nerd-fonts-fantasque-sans-mono ttf-meslo-nerd-font-powerlevel10k ttf-joypixels tela-icon-theme ksysguard plasma5-applets-netspeed telegram-desktop-bin
+yay --noconfirm -S spotify-adblock google-chrome brave-bin visual-studio-code-bin sublime-text-4 nerd-fonts-fantasque-sans-mono ttf-meslo-nerd-font-powerlevel10k ttf-joypixels ksysguard plasma5-applets-netspeed telegram-desktop-bin
 
 
 /bin/echo -e "fix sudo privileges after reboot"
